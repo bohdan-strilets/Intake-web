bdb39b3e5d7267b0c3fa04b05a72a6a5847d6a97
BS <bohdan.strilets@gmail.com>
2026-02-03 12:12:06 +0100

    refactor(stack): add reimport and change type for AS props

--- src/shared/ui/layout/Stack/Stack.types.ts
+++ src/shared/ui/layout/Stack/Stack.types.ts
@@ -1,28 +1,16 @@
 import type { RecipeVariants } from '@vanilla-extract/recipes';
 import type { HTMLAttributes, ReactNode } from 'react';
 
+import type { StructuralElement } from '@shared/types';
+
 import type { root } from './Stack.css';
 
 export type StackVariants = RecipeVariants<typeof root>;
 
-type StackAs =
-  | 'div'
-  | 'section'
-  | 'article'
-  | 'main'
-  | 'nav'
-  | 'header'
-  | 'footer'
-  | 'aside'
-  | 'ul'
-  | 'ol'
-  | 'li'
-  | 'form';
-
 export type Props = {
   children: ReactNode;
   className?: string;
-  as?: StackAs;
+  as?: StructuralElement;
 };
 
 export type StackProps = Props & Omit<HTMLAttributes<HTMLElement>, 'className'> & StackVariants;
