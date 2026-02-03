ce26cb94f51bdba04e6b0f8167c86d54c4ee626e
BS <bohdan.strilets@gmail.com>
2026-02-03 12:13:10 +0100

    feat(types): add shared type structural elements

---
+++ src/shared/types/index.ts
@@ -0,0 +1,1 @@
+export type { StructuralElement } from './structural-element.type';
---
+++ src/shared/types/structural-element.type.ts
@@ -0,0 +1,13 @@
+export type StructuralElement =
+  | 'div'
+  | 'section'
+  | 'article'
+  | 'main'
+  | 'nav'
+  | 'header'
+  | 'footer'
+  | 'aside'
+  | 'ul'
+  | 'ol'
+  | 'li'
+  | 'form';
