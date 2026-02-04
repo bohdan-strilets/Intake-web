0a6d959f63b100db4eaeb967ad441b96092f3bba
BS <bohdan.strilets@gmail.com>
2026-02-04 13:36:00 +0100

    feat(layout): add appHeader with logo

--- src/app/router/RootLayout.tsx
+++ src/app/router/RootLayout.tsx
@@ -2,11 +2,12 @@ import { Outlet } from '@tanstack/react-router';
 
 import { AppShell } from '@app/layouts/AppShell';
 
+import { AppHeader } from '@widgets/AppHeader';
 import { BottomNav } from '@widgets/BottomNav';
 
 export const RootLayout = () => {
   return (
-    <AppShell header={<div>Intake</div>} navigation={<BottomNav />}>
+    <AppShell header={<AppHeader />} navigation={<BottomNav />}>
       <Outlet />
     </AppShell>
   );
---
+++ src/widgets/AppHeader/AppHeader.tsx
@@ -0,0 +1,10 @@
+import { Logo } from '@shared/ui/brand/Logo';
+import { Inline } from '@shared/ui/layout/Inline';
+
+export const AppHeader = () => {
+  return (
+    <Inline>
+      <Logo size="md" />
+    </Inline>
+  );
+};
