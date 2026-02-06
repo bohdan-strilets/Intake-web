140e2f41155f4a2b617ab4e6029ae87779dcde16
BS <bohdan.strilets@gmail.com>
2026-02-06 21:53:30 +0100

    feat(hooks): implement outside click detection hook

---
+++ src/shared/hooks/click-outside/useClickOutside.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+
+import type { AnyEvent } from './click-outside.types';
+
+export const useClickOutside = <T extends HTMLElement>(
+  ref: React.RefObject<T | null>,
+  handler: (event: AnyEvent) => void,
+  enabled = true,
+) => {
+  useEffect(() => {
+    if (!enabled) return;
+
+    const listener = (event: AnyEvent) => {
+      const element = ref.current;
+
+      if (!element) return;
+      if (element.contains(event.target as Node)) return;
+
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler, enabled]);
+};
