8ae8cfbf76d5696337f1689b7d575df3e4cdc044
BS <bohdan.strilets@gmail.com>
2026-02-06 22:03:46 +0100

    feat(auth): add registration form

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,3 +1,20 @@
+import { RegisterForm } from '@features/auth/register/ui';
+
+import { ROUTES } from '@shared/routes';
+import { TextLink } from '@shared/ui/controls/TextLink';
+import { Card } from '@shared/ui/layout/Card';
+import { Title } from '@shared/ui/typography/Title';
+
 export const RegisterPage = () => {
-  return <div>Register</div>;
+  return (
+    <Card gap="xl">
+      <Title level={1} size="lg">
+        Register
+      </Title>
+
+      <RegisterForm />
+
+      <TextLink to={ROUTES.auth.login}>Already have an account?</TextLink>
+    </Card>
+  );
 };
