bbcb17d4c17218261444772908b3dad835c28a23
BS <bohdan.strilets@gmail.com>
2026-02-08 18:41:15 +0100

    feat(calendar): create dashboard page with calendar

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,56 @@
+import { useNavigate } from '@tanstack/react-router';
+import { useMemo } from 'react';
+
+import { getMonthMatrix, useCalendarNavigation } from '@widgets/Calendar/model';
+import { Calendar, CalendarMonthHeader } from '@widgets/Calendar/ui';
+
+import { mapCaloriesByDate } from '@features/calendar/mappers';
+import { useCalendarMonth } from '@features/calendar/model';
+
+import { formatMonthLabel } from '@shared/lib/date';
+import { ROUTES } from '@shared/routes';
+import { Spinner } from '@shared/ui/feedback/Spinner';
+import { Divider } from '@shared/ui/layout/Divider';
+
 export const DashboardPage = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+
+  const { year, month, monthParam, goPrevMonth, goNextMonth } =
+    useCalendarNavigation();
+
+  const { data = [], isLoading } = useCalendarMonth({ month: monthParam });
+
+  const matrix = useMemo(() => getMonthMatrix(year, month), [year, month]);
+
+  const handleDayClick = (date: string) => {
+    navigate({
+      to: ROUTES.app.day,
+      params: { date },
+    });
+  };
+
+  const monthLabel = formatMonthLabel(year, month, 'uk-UA');
+  const caloriesByDate = useMemo(() => mapCaloriesByDate(data), [data]);
+
+  if (isLoading) {
+    return <Spinner size="lg" />;
+  }
+
+  return (
+    <>
+      <CalendarMonthHeader
+        label={monthLabel}
+        onPrev={goPrevMonth}
+        onNext={goNextMonth}
+      />
+
+      <Divider />
+
+      <Calendar
+        matrix={matrix}
+        onDayClick={handleDayClick}
+        caloriesByDate={caloriesByDate}
+      />
+    </>
+  );
 };
