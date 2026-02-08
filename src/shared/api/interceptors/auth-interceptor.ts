9f98f26c633d321a6e4c01ac554072a2a69449a0
BS <bohdan.strilets@gmail.com>
2026-02-08 11:41:05 +0100

    feat(auth): read token from localStorage in interceptor

--- src/shared/api/interceptors/auth-interceptor.ts
+++ src/shared/api/interceptors/auth-interceptor.ts
@@ -1,12 +1,14 @@
 import type { AxiosInstance } from 'axios';
 
+import { tokenStorage } from '@shared/lib/token-storage';
+
 export function setupAuthInterceptor(api: AxiosInstance) {
   api.interceptors.request.use((config) => {
-    // const token = get
+    const token = tokenStorage.get();
 
-    // if (token) {
-    //   config.headers.Authorization = `Bearer ${token}`;
-    // }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
     return config;
   });
