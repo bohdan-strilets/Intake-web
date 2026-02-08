25d1b36965cbd13fd1499d40325ec0bc5de2f6f5
BS <bohdan.strilets@gmail.com>
2026-02-08 13:07:08 +0100

    refactor(auth): moved schema and api to separate folders

--- src/features/auth/login/api/loginUser.ts
+++ src/features/auth/login/api/loginUser.ts
@@ -1,7 +1,7 @@
 import { API_ROUTES } from '@shared/api';
 import { post } from '@shared/api/http';
 
-import type { LoginFormValues, LoginResponse } from './types';
+import type { LoginFormValues, LoginResponse } from '../types';
 
 export const loginUser = async (userData: LoginFormValues) => {
   return await post<LoginResponse, LoginFormValues>(
--- src/features/auth/register/api/registerUser.ts
+++ src/features/auth/register/api/registerUser.ts
@@ -3,7 +3,7 @@ import type { UserResponse } from '@entities/user/types';
 import { API_ROUTES } from '@shared/api';
 import { post } from '@shared/api/http';
 
-import type { RegisterFormValues } from './types';
+import type { RegisterFormValues } from '../types';
 
 export const registerUser = async (userData: RegisterFormValues) => {
   return await post<UserResponse, RegisterFormValues>(
