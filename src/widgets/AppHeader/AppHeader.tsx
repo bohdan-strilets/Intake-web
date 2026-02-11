eb135e1df87c5e34313d17586e6121234aed4b76
BS <bohdan.strilets@gmail.com>
2026-02-11 16:13:06 +0100

    refactor(shell): add container to appHeader

--- src/widgets/AppHeader/AppHeader.tsx
+++ src/widgets/AppHeader/AppHeader.tsx
@@ -1,13 +1,16 @@
 import { ThemeToggle } from '@widgets/ThemeToggle';
 
 import { Logo } from '@shared/ui/brand/Logo';
+import { Container } from '@shared/ui/layout/Container';
 import { Inline } from '@shared/ui/layout/Inline';
 
 export const AppHeader = () => {
   return (
-    <Inline justify="between">
-      <Logo size="md" />
-      <ThemeToggle />
-    </Inline>
+    <Container>
+      <Inline justify="between">
+        <Logo size="md" />
+        <ThemeToggle />
+      </Inline>
+    </Container>
   );
 };
