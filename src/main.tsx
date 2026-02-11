b38d1a4e2db8f42948766be8ce2ac81b1e5f7fdc
BS <bohdan.strilets@gmail.com>
2026-02-11 16:19:18 +0100

    chore(app): add reset style

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import { AppProvider } from '@app/providers/AppProvider';
 
+import '@shared/styles/reset.css';
 import '@shared/styles/globals.css';
 
 const root = document.getElementById('root') as HTMLDivElement;
