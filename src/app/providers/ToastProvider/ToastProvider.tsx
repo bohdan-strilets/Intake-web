850cf4c8c9e95c1f5b7dbee3eb2262c9f67e0dd8
BS <bohdan.strilets@gmail.com>
2026-02-11 22:50:26 +0100

    refactor(toast): change styles

--- src/app/providers/ToastProvider/ToastProvider.tsx
+++ src/app/providers/ToastProvider/ToastProvider.tsx
@@ -2,12 +2,6 @@ import { Toaster } from 'sonner';
 
 export function ToastProvider() {
   return (
-    <Toaster
-      position="top-right"
-      richColors
-      closeButton
-      duration={4000}
-      expand={false}
-    />
+    <Toaster position="top-center" richColors duration={4000} expand={false} />
   );
 }
