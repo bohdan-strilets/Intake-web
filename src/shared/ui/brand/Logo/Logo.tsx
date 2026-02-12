247adc535005490fe14a4d486caf99ce36706dd3
BS <bohdan.strilets@gmail.com>
2026-02-12 19:42:33 +0100

    fix(ui): correct path for logo img

--- src/shared/ui/brand/Logo/Logo.tsx
+++ src/shared/ui/brand/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import type { LogoProps } from './Logo.types';
 export function Logo({ size, className }: LogoProps) {
   return (
     <img
-      src="logo.webp"
+      src="/logo.webp"
       alt="Company Logo"
       title="Intake"
       className={clsx(root({ size }), className)}
