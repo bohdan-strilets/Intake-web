78a618e42c57c7a45578f9ff6bd6b3cb26ebcdbf
BS <bohdan.strilets@gmail.com>
2026-02-19 09:16:51 +0100

    refactor(hook): replace mouseDown with click

--- src/shared/hooks/clickOutside/useClickOutside.ts
+++ src/shared/hooks/clickOutside/useClickOutside.ts
@@ -19,11 +19,11 @@ export const useClickOutside = <T extends HTMLElement>(
       handler(event);
     };
 
-    document.addEventListener('mousedown', listener);
+    document.addEventListener('click', listener);
     document.addEventListener('touchstart', listener);
 
     return () => {
-      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('click', listener);
       document.removeEventListener('touchstart', listener);
     };
   }, [ref, handler, enabled]);
