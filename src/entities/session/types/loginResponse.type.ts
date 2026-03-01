1251d6ef2ffece49527cba293772e1c48ad9d2bc
BS <bohdan.strilets@gmail.com>
2026-03-01 04:53:38 +0100

    refactor(auth): remove unused LoginResponse type definition to streamline authentication code

--- src/entities/session/types/loginResponse.type.ts
+++
@@ -1,8 +0,0 @@
-import type { UserEntity } from '@entities/user';
-
-import type { AuthResponse } from './authResponse.type';
-
-export type LoginResponse = {
-  tokens: AuthResponse;
-  user: UserEntity;
-};
